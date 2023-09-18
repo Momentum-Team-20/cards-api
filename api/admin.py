59ea0fcdbd5a94d9df3b51fd691cac67cc59d0c6
Amy Gori <amydgori@gmail.com>
2023-09-17 21:20:12 -0400

    Add updated models

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Card, Follow
+from .models import User, Card, FollowRelationship
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Card)
-admin.site.register(Follow)
+admin.site.register(FollowRelationship)
--- api/models.py
+++ api/models.py
@@ -3,7 +3,51 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    followed_users = models.ManyToManyField(
+        "self",
+        through="FollowRelationship",
+        related_name="followed_by",
+        symmetrical=False,
+    )
+
+    def follow_another_user(self, other_user):
+        relationship, created = FollowRelationship.objects.get_or_create(
+            follower=self, followed_user=other_user
+        )
+        return relationship
+
+    def unfollow_another_user(self, other_user):
+        FollowRelationship.objects.filter(
+            follower=self, followed_user=other_user
+        ).delete()
+
+    def block_follower(self, other_user):
+        relationship = FollowRelationship.objects.get(
+            follower=other_user, followed_user=self
+        )
+        relationship.status = FollowRelationship.Status.BLOCKED
+        relationship.save()
+        return relationship
+
+    def unblock_follower(self, other_user):
+        relationship = FollowRelationship.objects.get(
+            follower=other_user, followed_user=self
+        )
+        relationship.status = FollowRelationship.Status.FOLLOWING
+        relationship.save()
+        return relationship
+
+    def get_followed_users(self):
+        return self.following.filter(status=FollowRelationship.Status.FOLLOWING)
+
+    def get_followers(self):
+        return self.followers.filter(status=FollowRelationship.Status.FOLLOWING)
+
+    def get_users_blocking_me(self):
+        return self.following.filter(status=FollowRelationship.Status.BLOCKED)
+
+    def get_blocked_followers(self):
+        return self.followers.filter(status=FollowRelationship.Status.BLOCKED)
 
 
 class BaseModel(models.Model):
@@ -20,15 +64,34 @@ class Card(BaseModel):
     back_text = models.CharField(max_length=255, null=True, blank=True)
     background_color = models.CharField(max_length=255)
     font = models.CharField(max_length=255, null=True, blank=True)
+    draft = models.BooleanField(
+        default=False
+    )  # false because front end may not implemet draft feature
 
     def __str__(self):
-      return f"Card: {self.front_text}"
+        return f"Card: {self.front_text}"
 
 
-class Follow(models.Model):
-    user_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follows_as_follower")
-    user_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follows_as_followed_user")
+class FollowRelationship(models.Model):
+    class Status(models.IntegerChoices):
+        FOLLOWING = (1, "Following")
+        BLOCKED = (0, "Blocked")
+
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+    followed_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+    status = models.IntegerField(choices=Status.choices, default=Status.FOLLOWING)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user_from.username} follows {self.user_to.username}"
+        return f"{self.follower.username} follows {self.followed_user.username}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["follower", "followed_user"], name="unique_follows"
+            )
+        ]
